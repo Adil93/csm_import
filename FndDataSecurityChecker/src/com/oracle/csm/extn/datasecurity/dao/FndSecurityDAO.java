25f99ab862ee1a06c354e9049e8f78d75fe8562c
Adil Muthukoya <adil93.mec@gmail.com>
2018-03-07 22:41:53 +0530

    Hibernate Conifg added

--- FndDataSecurityChecker/src/com/oracle/csm/extn/datasecurity/dao/FndSecurityDAO.java
+++ FndDataSecurityChecker/src/com/oracle/csm/extn/datasecurity/dao/FndSecurityDAO.java
@@ -2,52 +2,24 @@ package com.oracle.csm.extn.datasecurity.dao;
 
 import java.util.List;
 
-import javax.persistence.criteria.CriteriaBuilder;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.*;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
 import com.oracle.csm.extn.datasecurity.domain.FndObject;
+import com.oracle.csm.extn.datasecurity.utils.HibernateUtil;
 
 public class FndSecurityDAO {
 
-	private static SessionFactory sessionFactory;
-	private static ServiceRegistry serviceRegistry;
+	public List<FndObject> getFndObject(FndObject fndObj) {
+		Session s = HibernateUtil.getSessionFactory().openSession();
+		Criteria cr = s.createCriteria(FndObject.class);
+		cr.add(Restrictions.eq("objName", fndObj.getObjName()));
+		List<FndObject> results = cr.list();
 
-	private static Session getSession() {
-		Session s = null;
-        if(s == null)
-        { 	
-        	Configuration configuration = new Configuration();
-        	configuration.configure();
-        	ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties())
-				.buildServiceRegistry();
-        	sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-        	s = sessionFactory.openSession();
-        	return s;
-        }
-        else
-        	return s;
+		System.out.println("FndObject " + results.get(0).getObjName());
+		return results;
 	}
-	    
-    public List<FndObject> getFndObject(FndObject fndObj) {
-    	Session s = getSession();
-    	Criteria cr = s.createCriteria(FndObject.class);
-    	cr.add(Restrictions.eq("objName", fndObj.getObjName()));   
-    	List<FndObject> results = cr.list();
-
-       //	FndObject fndObject= (FndObject) s.get(FndObject.class, fndObj.getObjectId());
-
-    	System.out.println("FndObject "+results.get(0).getObjName());
-    	return results;
-    }
-    
 
 }
-
